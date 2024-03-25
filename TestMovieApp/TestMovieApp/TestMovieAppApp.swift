457b555350b2b37007d875de4493a4222258ade9
Andrey Barsukov <108578685+isNotAndy@users.noreply.github.com>
2024-03-25 14:40:21 +0300

    initial commit

---
+++ TestMovieApp/TestMovieApp/TestMovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestMovieAppApp.swift
+//  TestMovieApp
+//
+//  Created by Andrey Barsukov on 25.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestMovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
